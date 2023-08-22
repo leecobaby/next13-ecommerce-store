5c1bff81e80f65dd0a5bb0f061d0b79b0aff34e4
Leeco <leeco19@qq.com>
2023-08-22 10:02:01 +0000

    feat: update home page

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,8 +1,25 @@
-const HomePage = () => {
+import { getBillboard } from '@/actions/get-billboard'
+import { getProducts } from '@/actions/get-products'
+import { Billboard } from '@/components/billboard'
+import { ProductList } from '@/components/product-list'
+import { Container } from '@/components/ui/container'
+
+export const revalidate = 0
+
+const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true })
+  const billboard = await getBillboard('725fdc17-9343-4ca8-80e5-d08ef942c034')
+
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
+      </div>
+    </Container>
   )
 }
 
